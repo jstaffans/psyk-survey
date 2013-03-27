e4071ec0f35e8958f1b34937416b6e00858fd968
jstaffans <johannes.staffans@gmail.com>
2013-03-27 14:09:35 +0200

    more setup

--- puppet/appserver.pp
+++ puppet/appserver.pp
@@ -9,5 +9,8 @@ exec { 'apt-get update':
 include java
 
 class { 'tomcat7':
-  http_port => 8080
+  http_port => 8080,
+  mysql_user => 'dbuser',
+  mysql_password => 'dbuser',
+  mysql_url => 'jdbc:mysql://33.33.33.11/psykdb',
 }
